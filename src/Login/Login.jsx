b638e2c18add29785d841984f857cfb7172706dd
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-03 21:28:09 +0100

    input value form done

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
-    const handleLogin = e =>{
+    const handleLogin = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
@@ -21,22 +22,24 @@ const Login = () => {
                                 <label className="label">
                                 <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                 <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                                 <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button type="submit" className="btn btn-primary">Login</button>
                             </div>
                         </form>
-                    
+                        <p className="ml-5">New here, need a account ? <Link>
+                        <button className="btn btn-link">Register</button>
+                        </Link> </p>
                 </div>
             </div>
         </div>
