16f1a05325d9616e9b13605e44dd5fa0a0553adb
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-04 20:35:05 +0100

    register - to -login page updated

--- src/Registration/Registration.jsx
+++ src/Registration/Registration.jsx
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 const Registration = () => {
     const handleRegister = e => {
         e.preventDefault();
-
+        const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+        console.log(name, email, password);
     }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <h1 className="text-5xl font-bold">Register now!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -24,6 +24,12 @@ const Registration = () => {
                                 <span className="label-text">Email</span>
                                 </label>
                                 <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name="name" placeholder="enter your name" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
