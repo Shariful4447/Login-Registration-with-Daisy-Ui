dc0730584ef4ea330b8764d0d99a97fdf999ac26
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-04 20:29:08 +0100

    register - to -login page route declare

--- src/Registration/Registration.jsx
+++ src/Registration/Registration.jsx
@@ -1,40 +1,46 @@
+import { Link } from "react-router-dom";
 
 
 const Registration = () => {
+    const handleRegister = e => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="hero-content flex-col">
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Login now!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <form className="card-body">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Enter Your Name</span>
-                        </label>
-                        <input type="text" placeholder="name" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Enter Your Email</span>
-                        </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Enter Your Password Password</span>
-                        </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
-                        <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn btn-primary">Create Account</button>
-                    </div>
-                </form>
+                  
+                        <form onSubmit={handleRegister} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button type="submit" className="btn btn-primary">Register Now</button>
+                            </div>
+                        </form>
+                        <p className="ml-5">Already have account, LOGIN here <Link to="/Login">
+                        <button className="btn btn-link">Login</button></Link> 
+                         </p>
                 </div>
             </div>
         </div>
